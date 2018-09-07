603fa6bbe357b7e6e2881fc7086d59b55784dce0
Alexey Gorshkov <agorshkov23@mail.ru>
2018-09-08 00:30:34 +0300

    added icecast to json

---
+++ scripts/icecast2json.ts
@@ -0,0 +1,11 @@
+interface IcecastStream {
+    mountPoint: string;
+    title: string;
+    description: string;
+    contentType: string;
+    bitrate: number;
+}
+
+function parseStreams() {
+    const $streams = document.getElementsByClassName("newscontent");
+}
